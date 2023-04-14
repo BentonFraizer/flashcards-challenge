7be8ac2f9219e62bd22c46502e8c7b0d911d1386
Maxim <maxbeat040@gmail.com>
2023-04-14 10:23:46 +0300

    Реализована database

---
+++ db/models/question.js
@@ -0,0 +1,40 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Question extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Theme }) {
+      this.belongsTo(Theme, { foreignKey: 'theme_id' });
+    }
+  }
+  Question.init(
+    {
+      theme_id: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Theme',
+          key: 'id',
+        },
+      },
+      questions: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      answers: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Question',
+    }
+  );
+  return Question;
+};

4a3a0d8f41b41df0eac88dcb2174ac5325566466
Yuri Lopukhin_1 <yurilopukhinfol@mail.ru>
2023-04-14 10:27:51 +0300

    Внесены мелкие правки

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,7 +4,7 @@ module.exports = function Layout({ children }) {
   return (
     <html lang="ru">
       <head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="/css/normalize.css" />
